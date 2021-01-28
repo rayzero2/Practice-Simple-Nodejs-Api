e8e064a2ab4fe4a8bd8c9887d411b8d2bb8cf24c
Patanasak <patanasak.lek@,mail.kmutt.ac.th>
2021-01-28 16:03:58 +0700

    fix

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+provider "azurerm" {
+  version = ">=1.15.0"
+}
+
 resource "azurerm_resource_group" "INT493" {
     name = "lab1"
     location = "Southeast Asia"
+}
+
+resource "azurerm_virtual_network" "INT493" {
+    name = "INT493-vnet"
+    resource_group_name = "${azurerm_resource_group.int493.name}"
+    location = "Southeast Asia"
+    address_space = [ "10.0.0.0/24" ]
 }
